884ab3afbb25b2bd47a16bde01960e3877c99588
長谷川裕一 <hase@starlight-storm.com>
2017-07-10 23:47:40 +0900

    ValueHelperの修正など.

--- tmc/src/main/java/sample/util/values/ValuesHelper.java
+++ tmc/src/main/java/sample/util/values/ValuesHelper.java
@@ -1,82 +1,79 @@
 package sample.util.values;
 
-import java.io.IOException;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import com.google.common.reflect.ClassPath;
 
 @Component("vh")
-@Lazy
 public class ValuesHelper {
 	@Value("${app.values}")
-	private static String values;
+	private String values;
 
-    private final Map<String, String> valuesObjList;
+	@PostConstruct
+	public void postConstructSetValues() throws Exception {
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		valuesObjList = ClassPath.from(loader).getTopLevelClassesRecursive(values).stream().filter(classInfo -> {
+			try {
+				Class<?> clazz = Class.forName(classInfo.getName());
+				return !clazz.equals(Values.class) && Values.class.isAssignableFrom(clazz);
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}).collect(Collectors.toMap(ClassPath.ClassInfo::getSimpleName, ClassPath.ClassInfo::getName));
+	}
 
-    private ValuesHelper() throws IOException {
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        // valuesObjList = ClassPath.from(loader).getTopLevelClassesRecursive(values)
-        valuesObjList = ClassPath.from(loader).getTopLevelClassesRecursive("sample.customer.util.values")
-                .stream()
-                .filter(classInfo -> {
-                    try {
-                        Class<?> clazz = Class.forName(classInfo.getName());
-                        return !clazz.equals(Values.class) && Values.class.isAssignableFrom(clazz);
-                    } catch (ClassNotFoundException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .collect(Collectors.toMap(ClassPath.ClassInfo::getSimpleName, ClassPath.ClassInfo::getName));
-    }
+	private Map<String, String> valuesObjList;
 
-    @SuppressWarnings("unchecked")
-    public <T extends Enum<T> & Values> String getValue(String classSimpleName, String valueName)
-            throws ClassNotFoundException {
-        Class<T> enumType = (Class<T>) Class.forName(this.valuesObjList.get(classSimpleName));
-        T val = Enum.valueOf(enumType, valueName);
-        return val.getValue();
-    }
+	private ValuesHelper() {}
 
-    public <T extends Enum<T> & Values> String getValue(Class<T> enumType, String valueName) {
-        T val = Enum.valueOf(enumType, valueName);
-        return val.getValue();
-    }
+	@SuppressWarnings("unchecked")
+	public <T extends Enum<T> & Values> String getValue(String classSimpleName, String valueName)
+			throws ClassNotFoundException {
+		Class<T> enumType = (Class<T>) Class.forName(this.valuesObjList.get(classSimpleName));
+		T val = Enum.valueOf(enumType, valueName);
+		return val.getValue();
+	}
 
-    @SuppressWarnings("unchecked")
-    public <T extends Enum<T> & Values> String getText(String classSimpleName, String value)
-            throws ClassNotFoundException {
-        Class<T> enumType = (Class<T>) Class.forName(this.valuesObjList.get(classSimpleName));
-        String result = "";
-        for (T val : enumType.getEnumConstants()) {
-            if (val.getValue().equals(value)) {
-                result = val.getText();
-                break;
-            }
-        }
-        return result;
-    }
+	public <T extends Enum<T> & Values> String getValue(Class<T> enumType, String valueName) {
+		T val = Enum.valueOf(enumType, valueName);
+		return val.getValue();
+	}
 
-    public <T extends Enum<T> & Values> String getText(Class<T> enumType, String value) {
-        String result = "";
-        for (T val : enumType.getEnumConstants()) {
-            if (val.getValue().equals(value)) {
-                result = val.getText();
-                break;
-            }
-        }
-        return result;
-    }
+	@SuppressWarnings("unchecked")
+	public <T extends Enum<T> & Values> String getText(String classSimpleName, String value)
+			throws ClassNotFoundException {
+		Class<T> enumType = (Class<T>) Class.forName(this.valuesObjList.get(classSimpleName));
+		String result = "";
+		for (T val : enumType.getEnumConstants()) {
+			if (val.getValue().equals(value)) {
+				result = val.getText();
+				break;
+			}
+		}
+		return result;
+	}
 
-    @SuppressWarnings("unchecked")
-    public <T extends Enum<T> & Values> T[] values(String classSimpleName)
-            throws ClassNotFoundException {
-        Class<T> enumType = (Class<T>) Class.forName(this.valuesObjList.get(classSimpleName));
-        return enumType.getEnumConstants();
-    }
+	public <T extends Enum<T> & Values> String getText(Class<T> enumType, String value) {
+		String result = "";
+		for (T val : enumType.getEnumConstants()) {
+			if (val.getValue().equals(value)) {
+				result = val.getText();
+				break;
+			}
+		}
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T extends Enum<T> & Values> T[] values(String classSimpleName) throws ClassNotFoundException {
+		Class<T> enumType = (Class<T>) Class.forName(this.valuesObjList.get(classSimpleName));
+		return enumType.getEnumConstants();
+	}
 
 }
